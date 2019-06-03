e6e81e931704a666f10759ee70e4d0b09854b8ad
Samantha Waln <samywaln@gmail.com>
2019-06-02 22:17:25 -0700

    JavaScript file added

---
+++ project_functions.js
@@ -0,0 +1,13 @@
+function changeProjectPrev() {
+  var project = document.querySelector("#change-color-left");
+  project.style.backgroundColor = 'gray';
+  var project2 = document.querySelector("change-color-right");
+  project2.style.backgroundColor = #dcd0ff;
+}
+
+function changeProjectNext() {
+  var project = document.querySelector("#change-color-right");
+  project.style.backgroundColor = 'gray';
+  var project2 = document.querySelector("#change-color-left");
+  project2.style.backgroundColor = #dcd0ff;
+}
